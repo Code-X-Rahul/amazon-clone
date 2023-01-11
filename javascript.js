4f1bfc21acce6536f7ef5ab7cc2257b1a35e0df3
Rahul Rajput <121877628+Code-X-Rahul@users.noreply.github.com>
2023-01-11 21:01:09 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let a  = "i am rahul";
+console.log(a);
+const first = "yoyo";
+// first = "yyoyooyyo"; this cannot be changed because it is constant variable
+console.log(first);
+let  age = 20;
+if(age>18){
+    console.log("you can have segs");
+}
+else if(age<18){
+    console.log("you can shake it shake it");
+}
